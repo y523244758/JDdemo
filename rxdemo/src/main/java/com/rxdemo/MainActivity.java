4f3e44e73312df67d8b16616dea8ed9c057f5416
hjy <523244758@qq.com>
2017-11-23 11:14:58 +0800

    添加视频 添加了轮播

--- rxdemo/src/main/java/com/rxdemo/MainActivity.java
+++ rxdemo/src/main/java/com/rxdemo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rxdemo;
 
 import android.os.Environment;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.IdRes;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
 import com.rxdemo.Model.News;
 import com.rxdemo.Presenter.ShowPresenterSe;
 import com.rxdemo.dao.DownloadUtil;
 import com.rxdemo.view.ShowView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
@@ -32,15 +43,31 @@ public class MainActivity extends AppCompatActivity implements ShowView{
     private int max;
     private DownloadUtil mDownloadUtil;
     String urlString = "http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4";
+
+    ViewPager vp;
+    RadioGroup rg;
+    List<String> ilist=new ArrayList<>();
+    int i;
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            vp.setCurrentItem(msg.what);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
         //获取资源ID
         videoView=(VideoView) findViewById(R.id.videoview);
         //创建MediaController
         final MediaController controller=new MediaController(this);
 
+        //轮播
+        vp= (ViewPager) findViewById(R.id.vp);
+        rg= (RadioGroup) findViewById(R.id.rg);
 
         tv= (TextView) findViewById(R.id.tv);
         total= (TextView) findViewById(R.id.textView);
@@ -109,9 +136,39 @@ public class MainActivity extends AppCompatActivity implements ShowView{
         jcVideoPlayerStandard= (JCVideoPlayerStandard) findViewById(R.id.jiecao_Player);
         jcVideoPlayerStandard.setUp(urlString,jcVideoPlayerStandard.SCREEN_LAYOUT_NORMAL,"播放视频");
 
+        //轮播
+        ilist.add("http://img01.vgtime.com/game/cover/2017/11/23/171123101558445.jpg");
+        ilist.add("http://img01.vgtime.com/game/cover/2017/11/23/171123093907243.jpg");
+        ilist.add("http://img01.vgtime.com/web/topic/2017/11/22/171122182851825.jpg");
 
+        Myviewpage mp=new Myviewpage(MainActivity.this,ilist);
 
+        vp.setAdapter(mp);
 
+        vp.setCurrentItem(ilist.size()*10000);
+
+        i=vp.getCurrentItem();
+
+        getpage();
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                while (true){
+                    i++;
+                    try {
+                        sleep(5000);
+                        handler.sendEmptyMessage(i);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }.start();
 
     }
 
@@ -134,5 +191,56 @@ public class MainActivity extends AppCompatActivity implements ShowView{
     protected void onPause() {
         super.onPause();
         JCVideoPlayer.releaseAllVideos();
+    }
+
+    public void getpage() {
+
+        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position%ilist.size()){
+                    case 0:
+                        rg.check(R.id.b1);
+                        break;
+                    case 1:
+                        rg.check(R.id.b2);
+                        break;
+                    case 2:
+                        rg.check(R.id.b3);
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId%ilist.size()){
+                    case R.id.b1:
+                        vp.setCurrentItem(0);
+                        break;
+                    case R.id.b2:
+                        vp.setCurrentItem(1);
+                        break;
+                    case R.id.b3:
+                        vp.setCurrentItem(2);
+                        break;
+                }
+            }
+        });
+
+
     }
 }
